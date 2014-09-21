bb9329de1589cefbb9850d6c32c411cf33728723
SandyMaciel <sandymaciel@greenmilesoftware.com>
2014-09-21 13:46:36 -0300

    Codigo

---
+++ config.lua
@@ -0,0 +1,14 @@
+application = {
+    content = {
+        width = 320,
+        height = 570, 
+        scale = "letterBox",
+        fps = 60
+        
+        --[[
+        imageSuffix = {
+            ["@2x"] = 2,
+        }
+        --]]
+    }
+}
